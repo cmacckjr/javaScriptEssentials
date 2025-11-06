a7ff4b10a70e499746a63c493add48fc963041ea
cmacckjr <cmacckjr@gmail.com>
2025-11-05 22:12:46 -0500

    Added scope files that work with variables and their scope

---
+++ Scope/scope_lab.js
@@ -0,0 +1,34 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+    // Block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let";
+console.log(globalConst); // Ouptut: "I'm a global constant"
+
+// Block scope
+console.log(blockVar); // Output: "I'm a block-scoped var"
+console.log(blockLet); // Throws ReferenceError
+console.log(blockConst); // Throws ReferenceError
+
+// This function is used to explore how function-scoped variable work
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
