3b7705f1c6be808978fa3a3ec590c2df48a729bc
cmacckjr <cmacckjr@gmail.com>
2025-11-07 17:24:24 -0500

    Added prices for breakfast menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,5 +1,5 @@
 // Initializing 3 arrays with the menu items
-const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
+const breakfastMenu = ["Pancakes - $12", "Eggs Benedict - $22.99", "Oatmeal - $21.99", "Frittata - $15"];
 const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
 const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
 
